efbb64f481fbcdbb98c715f65d092dad4423ab0f
flppetkovski <flp_petkovski@yahoo.com>
2021-01-08 23:59:27 +0100

    Final Code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react"
-import GlobalStyle from "./globalStyles"
-import {Navbar} from "./components"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from "./pages/HomePage/Home"
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
 
-
-const App = ()=> (
-  <Router>
-  <GlobalStyle/>
-  <Navbar/>
-  <Switch>
-  <Route path="/" exact component={Home} />
-  </Switch>
-  </Router>
-)
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import {homeObjOne} from "./Data"
-import {InfoSection} from "../../components/"
-const Home = () => {
- return (
-  <>
-   <InfoSection {...homeObjOne}/>
-  </>
- )
+import React from 'react';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import { InfoSection, Pricing } from '../../components';
+
+function Home() {
+  return (
+    <>
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjTwo} />
+      <Pricing />
+      <InfoSection {...homeObjFour} />
+    </>
+  );
 }
 
-export default Home
+export default Home;
